25e0a72c333d032ca2e9d892459f14984f51f048
mike411 <36472998-mike411@users.noreply.replit.com>
2024-11-01 20:06:19 +0000

    Agent query: Please verify: 1. Can you access the login page and see the 'Reset Password' link? 2. When clicking the reset password link, does it show the email input form?

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
+from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from database import db
 from models import User
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 app = Flask(__name__)
 
@@ -22,6 +25,27 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+def send_reset_email(user, token):
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    reset_url = url_for('reset_token', token=token, _external=True)
+    
+    message = Mail(
+        from_email='noreply@yourdomain.com',
+        to_emails=user.email,
+        subject='Password Reset Request',
+        html_content=f'''To reset your password, visit the following link:
+        <a href="{reset_url}">Reset Password</a>
+        
+        If you did not make this request, please ignore this email.
+        ''')
+    
+    try:
+        sg.send(message)
+        return True
+    except Exception as e:
+        print(f"Error sending email: {e}")
+        return False
 
 @app.route('/')
 def index():
@@ -68,6 +92,52 @@ def signup():
         return redirect(url_for('account'))
     return render_template('signup.html')
 
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('account'))
+    
+    if request.method == 'POST':
+        email = request.form['email']
+        user = User.query.filter_by(email=email).first()
+        
+        if user:
+            token = user.get_reset_token()
+            if send_reset_email(user, token):
+                flash('An email has been sent with instructions to reset your password.')
+            else:
+                flash('An error occurred. Please try again later.')
+        else:
+            flash('No account found with that email address.')
+        return redirect(url_for('login'))
+    
+    return render_template('reset_request.html')
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('account'))
+    
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('Invalid or expired reset token')
+        return redirect(url_for('reset_request'))
+    
+    if request.method == 'POST':
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        
+        if password != confirm_password:
+            flash('Passwords do not match')
+            return redirect(url_for('reset_token', token=token))
+        
+        user.password_hash = generate_password_hash(password)
+        db.session.commit()
+        flash('Your password has been updated!')
+        return redirect(url_for('login'))
+    
+    return render_template('reset_password.html')
+
 @app.route('/account')
 @login_required
 def account():
@@ -81,6 +151,6 @@ def logout():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.drop_all()  # Temporarily add this line to recreate tables
+        db.drop_all()  # Drop all tables to recreate them with new columns
         db.create_all()
     app.run(host='0.0.0.0', port=5000)
--- models.py
+++ models.py
@@ -1,11 +1,28 @@
 from flask_login import UserMixin
 from database import db
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flask import current_app
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(512))
+    reset_token = db.Column(db.String(100), unique=True)
+    reset_token_expires = db.Column(db.DateTime)
+
+    def get_reset_token(self):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
+
+    @staticmethod
+    def verify_reset_token(token, expires_sec=3600):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token, max_age=expires_sec)['user_id']
+            return User.query.get(user_id)
+        except:
+            return None
 
     def __repr__(self):
         return f'<User {self.username}>'
